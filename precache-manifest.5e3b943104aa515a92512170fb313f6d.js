61fb5698c95ec32f77e146d94f37559f718a026e
Hugo Perez <h.perez.saldvar@gmail.com>
2020-09-12 17:06:17 -0600

    Updates

---
+++ precache-manifest.5e3b943104aa515a92512170fb313f6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8720e6b2b1009bfd09e69bf9b70c894c",
+    "url": "/hperezs.github.io/mindelli-co/index.html"
+  },
+  {
+    "revision": "06c0c4b397969c449ba8",
+    "url": "/hperezs.github.io/mindelli-co/static/css/2.b7ede397.chunk.css"
+  },
+  {
+    "revision": "42b7a1eb1879bda8d085",
+    "url": "/hperezs.github.io/mindelli-co/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "06c0c4b397969c449ba8",
+    "url": "/hperezs.github.io/mindelli-co/static/js/2.47c011a5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/hperezs.github.io/mindelli-co/static/js/2.47c011a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42b7a1eb1879bda8d085",
+    "url": "/hperezs.github.io/mindelli-co/static/js/main.5bf6b8df.chunk.js"
+  },
+  {
+    "revision": "6c1b63045c9ffd254551",
+    "url": "/hperezs.github.io/mindelli-co/static/js/runtime-main.730cb92c.js"
+  }
+]);
