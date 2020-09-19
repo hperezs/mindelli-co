365a4aab4b58aa79e583ec1819ffc40fc813f922
Hugo Perez <h.perez.saldvar@gmail.com>
2020-09-18 23:22:01 -0600

    Added fade in animation

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -3,6 +3,13 @@ import { useParams } from 'react-router-dom';
 import { ShopContext } from '../context/shopContext';
 import { Loading } from '../components/Loading';
 import { Container, Row, Col, Div, Text, Button } from 'atomize';
+import styled from 'styled-components';
+import '../components/animations.css';
+
+const AnimationWrapper = styled.div`
+    animation-name: fadeIn;
+    animation-duration: 1s
+`;
 
 const ProductPage = () => {
     let { productId } = useParams();
@@ -32,53 +39,55 @@ const ProductPage = () => {
 
     return (
         <Container>
-            <Row>
-                <Col size={{ xs: "12", md: "1" }}>
-                    <Div d={{xs: "flex"}} flexDir={{xs: "row", md: "column"}} justify={{xs: "space-between"}} m={{t: {xs: "1rem", md: "0"}}}>
-                        {product.images.map(image => (
-                            <Div
-                                bgImg={image.src}
-                                bgSize="cover"
-                                bgPos="center center"
-                                h="6rem"
-                                m={{ t: { xs: "none", md: "2rem" } }}
-                                onClick={() => changeSelectedImage(image.src)}
-                                border={(selectedImage === image.src) ? '4px solid' : 'none'}
-                                borderColor='brand500'
-                                key={image.src}
-                                w={{xs: "5rem", md:"auto"}}
-                            >
-                            </Div>
-                        ))}
-                    </Div>
+            <AnimationWrapper>
+                <Row>
+                    <Col size={{ xs: "12", md: "1" }}>
+                        <Div d={{ xs: "flex" }} flexDir={{ xs: "row", md: "column" }} justify={{ xs: "space-between" }} m={{ t: { xs: "1rem", md: "0" } }}>
+                            {product.images.map(image => (
+                                <Div
+                                    bgImg={image.src}
+                                    bgSize="cover"
+                                    bgPos="center center"
+                                    h="6rem"
+                                    m={{ t: { xs: "none", md: "2rem" } }}
+                                    onClick={() => changeSelectedImage(image.src)}
+                                    border={(selectedImage === image.src) ? '4px solid' : 'none'}
+                                    borderColor='brand500'
+                                    key={image.src}
+                                    w={{ xs: "5rem", md: "auto" }}
+                                >
+                                </Div>
+                            ))}
+                        </Div>
 
-                </Col>
-                <Col size={{ xs: "12", md: "5" }}>
-                    <Div
-                        bgImg={selectedImage}
-                        bgSize="cover"
-                        bgPos="center center"
-                        h={{xs: "40vh", md: "30rem"}}
-                        m={{ t: "2rem" }}>
-                    </Div>
-                </Col>
-                <Col size={{ xs: "12", md: "5" }}>
-                    <Div
-                        m={{
-                            t: { xs: "1rem", md: "2rem" },
-                            l: { xs: "none", md: "1rem" }
-                        }}
-                    >
-                        <Text tag="h3" textSize="heading" textWeight="400" textColor="black500">{product.title}</Text>
-                        <Text tag="h5" textSize="title" textWeight="300" textColor="black400">${product.variants[0].price}</Text>
+                    </Col>
+                    <Col size={{ xs: "12", md: "5" }}>
+                        <Div
+                            bgImg={selectedImage}
+                            bgSize="cover"
+                            bgPos="center center"
+                            h={{ xs: "40vh", md: "30rem" }}
+                            m={{ t: "2rem" }}>
+                        </Div>
+                    </Col>
+                    <Col size={{ xs: "12", md: "5" }}>
+                        <Div
+                            m={{
+                                t: { xs: "1rem", md: "2rem" },
+                                l: { xs: "none", md: "1rem" }
+                            }}
+                        >
+                            <Text tag="h3" textSize="heading" textWeight="400" textColor="black500">{product.title}</Text>
+                            <Text tag="h5" textSize="title" textWeight="300" textColor="black400">${product.variants[0].price}</Text>
 
-                        <Text tag="p" textSize="subheader" m={{ t: {xs: "1rem", md: "4rem"}, b: "2rem" }}>{product.description}</Text>
-                        <Button bg="brand600" rounded="xs" onClick={() => addItemToCheckout(product.variants[0].id)} m={{b: "2rem"}}>
-                            Add to cart
+                            <Text tag="p" textSize="subheader" m={{ t: { xs: "1rem", md: "4rem" }, b: "2rem" }}>{product.description}</Text>
+                            <Button bg="brand600" rounded="xs" onClick={() => addItemToCheckout(product.variants[0].id)} m={{ b: "2rem" }}>
+                                Add to cart
                         </Button>
-                    </Div>
-                </Col>
-            </Row>
+                        </Div>
+                    </Col>
+                </Row>
+            </AnimationWrapper>
         </Container>
     );
 }
